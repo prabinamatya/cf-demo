b533c4dacb59bf10fc6937571729b05f98274b7f
Prabin Amatya <cloudtraining@CTIO-MBookPro-8.local>
2019-11-20 11:21:21 -0600

    initial manifest

---
+++ src/main/java/com/example/demo/controller/SampleRestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleRestController {
+    public static String version = "1.0.0";
+
+    @RequestMapping(value = "/version", method = {RequestMethod.GET, RequestMethod.POST})
+    public String version() {
+        return "Running version: " + version;
+    }
+
+    @RequestMapping(value = "/greeting", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return "hello " + name + "!";
+    }
+}
